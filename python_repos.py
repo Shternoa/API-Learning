8e85c2483e94e43dba5b1ded3efcde7c3003f450
Elman <134933773+Shternoa@users.noreply.github.com>
2023-06-14 11:41:43 +0300

    Работа со словарем ответа

--- python_repos.py
+++ python_repos.py
@@ -9,6 +9,26 @@ print('Status code: ', r.status_code)
 
 # Сохранение ответа
 repos_dict = r.json()
+print('Total repositories: ', repos_dict['total_count'])
 
-# Обработка результатов
-print(repos_dict.keys())
+# Анализ информации о репозиториях
+repos_dicts = repos_dict['items']
+print('Repositories returned: ', len(repos_dicts))
+
+# Анализ первого репозитория
+repos_dict = repos_dicts[1]
+print("\nKeys: ", len(repos_dict))
+for key in sorted(repos_dict.keys()):
+    print(key)
+# # Обработка результатов
+# print(repos_dict.keys())
+
+# Прочитаем значения некоторых ключей
+print("\nSelected information about first repository:")
+print('Name:', repos_dict['name'])
+print('Owner:', repos_dict['owner']['login'])
+print('Stars:', repos_dict['stargazers_count'])
+print('Repository:', repos_dict['html_url'])
+print('Created:', repos_dict['created_at'])
+print('Updated:', repos_dict['updated_at'])
+print('Description:', repos_dict['description'])
