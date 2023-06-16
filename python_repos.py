7c5c6a9ea723e5260419d225d9309aaaa2472be5
Elman <134933773+Shternoa@users.noreply.github.com>
2023-06-16 14:19:07 +0300

    Сводка популярных репозиториев

--- python_repos.py
+++ python_repos.py
@@ -16,19 +16,20 @@ repos_dicts = repos_dict['items']
 print('Repositories returned: ', len(repos_dicts))
 
 # Анализ первого репозитория
-repos_dict = repos_dicts[1]
-print("\nKeys: ", len(repos_dict))
-for key in sorted(repos_dict.keys()):
-    print(key)
+# repos_dict = repos_dicts[1]
+# print("\nKeys: ", len(repos_dict))
+# for key in sorted(repos_dict.keys()):
+#     print(key)
 # # Обработка результатов
 # print(repos_dict.keys())
 
 # Прочитаем значения некоторых ключей
-print("\nSelected information about first repository:")
-print('Name:', repos_dict['name'])
-print('Owner:', repos_dict['owner']['login'])
-print('Stars:', repos_dict['stargazers_count'])
-print('Repository:', repos_dict['html_url'])
-print('Created:', repos_dict['created_at'])
-print('Updated:', repos_dict['updated_at'])
-print('Description:', repos_dict['description'])
+for repos_dict in repos_dicts:
+    print("\nSelected information about first repository:")
+    print('Name:', repos_dict['name'])
+    print('Owner:', repos_dict['owner']['login'])
+    print('Stars:', repos_dict['stargazers_count'])
+    print('Repository:', repos_dict['html_url'])
+    print('Created:', repos_dict['created_at'])
+    print('Updated:', repos_dict['updated_at'])
+    print('Description:', repos_dict['description'])
